52b7150951d9a11cd8c7b45e1ae5d5f5c8721638
Stanislav Rogozhin <theptyza@gmail.com>
2019-07-05 16:44:42 +0300

    Pass id for pending tests

--- src/subprocess/reporter.ts
+++ src/subprocess/reporter.ts
@@ -25,66 +25,66 @@ export const getReporterFactory: ReporterFactory = (channel, debugSubprocess) =>
     private rootSuite: Suite;
     private currentTestIndex: number | null = null;
     private eventsCounter = 0;
-  
+
     constructor(runner: IRunner) {
       this.rootSuite = runner.suite as Suite;
-  
+
       runner.on('waiting', this.onRunnerWaiting);
       runner.on('start', this.onRunnerStart);
       runner.on('end', this.onRunnerEnd);
-  
+
       runner.on('suite', this.onRunnerSuTestart);
       runner.on('suite end', this.onRunnerSuiteEnd);
-  
+
       runner.on('test', this.onTestStart);
       runner.on('test end', this.onTestEnd);
-  
+
       runner.on('pass', this.onRunnerPass);
       runner.on('fail', this.onRunnerFail);
       runner.on('pending', this.onRunnerPending);
-  
+
       runner.on('hook', this.onRunnerHookStart);
       runner.on('hook end', this.onRunnerHookEnd);
     }
-  
+
     private onRunnerStart = () => {
       this.notifyParent('start');
     }
-  
+
     private onRunnerEnd = () => {
       this.notifyParent('end');
     }
-  
+
     private onRunnerSuTestart = (suite: Suite) => {
       const title = suite.root ? 'root' : suite.fullTitle();
       const id = getMessageId('suite', title, this.eventsCounter);
       suite[RUNNABLE_MESSAGE_CHANNEL_PROP] = id;
-  
+
       this.notifyParent('suite', { id });
       this.eventsCounter += 1;
     }
-  
+
     private onRunnerSuiteEnd = (suite: Suite) => {
       this.notifyParent('suite end', {
         id: suite[RUNNABLE_MESSAGE_CHANNEL_PROP],
       });
     }
-  
+
     private onRunnerWaiting = (/* rootSuite: Suite */) => {
       this.notifyParent('waiting');
     }
-  
+
     private onTestStart = (test: Test) => {
       const id = getMessageId('test', test.fullTitle(), this.eventsCounter);
       test[RUNNABLE_MESSAGE_CHANNEL_PROP] = id;
-  
+
       // this test is running for the first time, i.e. no retries for it have been executed yet
       if (this.currentTestIndex === null) {
         const currentTestIndex = test.parent.tests.indexOf(test);
         if (currentTestIndex === -1) {
           throw new Error('Could not find the test in the suite\'s tests');
         }
-  
+
         this.currentTestIndex = currentTestIndex;
       } else if (!test.parent.tests.includes(test)) {
         /**
@@ -97,28 +97,28 @@ export const getReporterFactory: ReporterFactory = (channel, debugSubprocess) =>
          */
         test.parent.tests[this.currentTestIndex] = test;
       }
-  
+
       this.runningTests.add(test);
-  
+
       this.notifyParent('test', { id });
       this.eventsCounter += 1;
     }
-  
+
     private onTestEnd = (test: Test) => {
       this.runningTests.delete(test);
       this.currentTestIndex = null;
-  
+
       this.notifyParent('test end', {
         id: test[RUNNABLE_MESSAGE_CHANNEL_PROP],
       });
     }
-  
+
     private onRunnerPass = (test: Test) => {
       this.notifyParent('pass', {
         id: test[RUNNABLE_MESSAGE_CHANNEL_PROP],
       });
     }
-  
+
     private onRunnerFail = (test: Test, err: Error) => {
       this.notifyParent('fail', {
         err: {
@@ -129,27 +129,27 @@ export const getReporterFactory: ReporterFactory = (channel, debugSubprocess) =>
         id: test[RUNNABLE_MESSAGE_CHANNEL_PROP],
       });
     }
-  
+
     private onRunnerPending = (test: Test) => {
-      this.notifyParent('pending', {
-        id: test[RUNNABLE_MESSAGE_CHANNEL_PROP],
-      });
+      const id = getMessageId('test', test.fullTitle(), this.eventsCounter);
+      test[RUNNABLE_MESSAGE_CHANNEL_PROP] = id;
+      this.notifyParent('pending', {id});
     }
-  
+
     private onRunnerHookStart = (hook: Hook) => {
       const id = hook[RUNNABLE_MESSAGE_CHANNEL_PROP] || getMessageId('hook', hook.title, this.eventsCounter);
       hook[RUNNABLE_MESSAGE_CHANNEL_PROP] = id;
-  
+
       this.notifyParent('hook', { id });
       this.eventsCounter += 1;
     }
-  
+
     private onRunnerHookEnd = (hook: Hook) => {
       this.notifyParent('hook end', {
         id: hook[RUNNABLE_MESSAGE_CHANNEL_PROP],
       });
     }
-  
+
     private notifyParent(event: string, data = {}) {
       if (debugSubprocess) {
         // eslint-disable-next-line no-console
@@ -158,7 +158,7 @@ export const getReporterFactory: ReporterFactory = (channel, debugSubprocess) =>
         this.notifyParentThroughIPC(event, data);
       }
     }
-  
+
     private notifyParentThroughIPC(event: string, data = {}) {
       // main process needs retried tests only when it starts
       // re-emitting subprocess test results, so it's safe to
@@ -171,7 +171,7 @@ export const getReporterFactory: ReporterFactory = (channel, debugSubprocess) =>
           });
         })
         : [];
-  
+
       // send the data snapshot with every event
       const snapshot: Snapshot = {
         data: {
@@ -184,7 +184,7 @@ export const getReporterFactory: ReporterFactory = (channel, debugSubprocess) =>
       };
 
       channel.sendEnsureDelivered(snapshot);
-  
+
       // and then send the event
       const reporterNotification: ReporterNotification = { event, data }
       channel.sendEnsureDelivered(reporterNotification);
